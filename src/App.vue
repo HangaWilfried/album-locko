a4fb7a4fd8884cbd1e79b7ebf382a553d378d1c5
HangaWilfried <wilfriedhanga5@gmail.com>
2021-11-22 10:09:32 +0100

    style: resize appearence on mobile 02

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,20 @@
 <template>
-  <div class=" bg-gray-50 h-full top-0 fixed w-full flex flex-col gap-5 items-center p-10">
-    <div class="pt-20">
-      <h1 class="flex flex-col gap-7 items-center">
-        <span class="text-6xl font-medium text-gray-400">Nouvel Album</span>
-        <span class="text-9xl font-thin text-pink-400">Locko</span>
-      </h1>
-    </div>
-    <div class="flex gap-5 py-20 items-center">
-      <div class="flex flex-col gap-5 items-center">
-        <input type="text" v-model="phoneNumber" placeholder="Entrez votre numero" class="text-gray-600 rounded border border-gray-200 h-10 px-2 focus:shadow-xl focus:outline-none focus:border-gray-600">
-        <div class="show" v-if="!isCorrect">
-          <ul v-for="(error, index) in errors" :key="index">
-            <li class="text-base list-disc text-pink-600">{{error}}</li>
-          </ul>
-        </div>
+  <div class=" bg-gray-50 h-full top-0 fixed w-full p-5">
+    <h1 class="flex flex-col items-center gap-2 mb-10 mt-10">
+      <span class="text-2xl font-medium text-gray-400">Nouvel Album</span>
+      <span class="text-2xl font-thin text-pink-400">Locko</span>
+    </h1>
+
+    <div class="flex flex-col place-content-start">
+      <div class="flex flex-col gap-7 mb-5">
+        <input type="text" v-model="phoneNumber"  class="pl-3 text-gray-600 rounded border border-gray-200 h-12 focus:shadow-xl focus:outline-none focus:border-gray-600 sm:w-full" placeholder="Entrez votre numero">
+        <button @click="checkValidation" class="w-full border border-blue-400 h-12 rounded bg-blue-400 text-white text-lg hover:bg-blue-500 hover:border-blue-500">Soumettre</button>
       </div>
-      <button @click="checkValidation" class="border border-blue-400 h-10 px-5 rounded bg-blue-400 text-white text-lg hover:bg-blue-500 hover:border-blue-500">Soumettre</button>
+      <template v-if="!isCorrect">
+        <ul>
+          <li v-for="(error, index) in errors" :key="index" class="mt-5 ml-4 text-base list-disc text-pink-600 mb-2">{{error}}</li>
+        </ul>
+      </template>
     </div>
   </div>
 
