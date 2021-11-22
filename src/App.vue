fcf45b2ce629f1480a70be0b04835d266b2ff7fe
HangaWilfried <wilfriedhanga5@gmail.com>
2021-11-22 09:18:48 +0100

    style: resize appearence on mobile

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class=" bg-gray-50 h-full top-0 fixed w-full flex flex-col gap-5 place-items-center">
+  <div class=" bg-gray-50 h-full top-0 fixed w-full flex flex-col gap-5 items-center p-10">
     <div class="pt-20">
       <h1 class="flex flex-col gap-7 items-center">
         <span class="text-6xl font-medium text-gray-400">Nouvel Album</span>
         <span class="text-9xl font-thin text-pink-400">Locko</span>
       </h1>
     </div>
-    <div class="flex gap-5 py-20 place-content-center">
-      <div class="flex flex-col gap-5 place-items-center">
-        <input type="text" v-model="phoneNumber" placeholder="Entrez votre numero" class="text-gray-600 rounded border border-gray-200 h-12 px-7 focus:shadow-xl focus:outline-none focus:border-gray-600">
+    <div class="flex gap-5 py-20 items-center">
+      <div class="flex flex-col gap-5 items-center">
+        <input type="text" v-model="phoneNumber" placeholder="Entrez votre numero" class="text-gray-600 rounded border border-gray-200 h-10 px-2 focus:shadow-xl focus:outline-none focus:border-gray-600">
         <div class="show" v-if="!isCorrect">
           <ul v-for="(error, index) in errors" :key="index">
             <li class="text-base list-disc text-pink-600">{{error}}</li>
           </ul>
         </div>
       </div>
-      <button @click="checkValidation" class="border border-blue-400 h-12 px-10 rounded bg-blue-400 text-white text-lg hover:bg-blue-500 hover:border-blue-500">Soumettre</button>
+      <button @click="checkValidation" class="border border-blue-400 h-10 px-5 rounded bg-blue-400 text-white text-lg hover:bg-blue-500 hover:border-blue-500">Soumettre</button>
     </div>
   </div>
 
